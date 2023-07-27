6caa40ebe4c97271d5a94e3b753310d8147c4446
owwwl666 <zaharsalam228@gmail.com>
2023-07-27 22:53:00 +1000

    Update script.py
    download books with titles.

--- script.py
+++ script.py
@@ -1,16 +1,39 @@
 import pathlib
-
+from pathvalidate import sanitize_filename
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
 
-folder = "books"
-pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+books_folder = "books"
+pathlib.Path(books_folder).mkdir(parents=True, exist_ok=True)
+
+
+def check_for_redirect(response):
+    if response != 200:
+        raise requests.exceptions.HTTPError
+
+
+def download_txt(book_link, book_id, folder):
+    soup = BeautifulSoup(requests.get(book_link).text, 'lxml')
+    filename = soup.find(id="content").find("h1").text.split('::')[0].strip()
+    book_name = sanitize_filename(filename)
+    book = Path(folder).joinpath(f"{book_id}.{book_name}.txt")
+    return book
+
 
 for id in range(1, 11):
-    url = f'https://tululu.org/txt.php?id={id}'
-    response = requests.get(url)
+    book_download_link = f'https://tululu.org/txt.php?id={id}'
+    response = requests.get(book_download_link, allow_redirects=False)
+    try:
+        check_for_redirect(response=response.status_code)
+    except requests.exceptions.HTTPError:
+        continue
+    else:
+        book = download_txt(
+            book_link=f'https://tululu.org/b{id}/',
+            book_id=id,
+            folder=books_folder
+        )
 
-    book = Path(folder).joinpath(f"id{id}")
-
-    with open(book, 'wb') as file:
-        file.write(response.content)
+        with open(book, 'wb') as file:
+            file.write(response.content)
