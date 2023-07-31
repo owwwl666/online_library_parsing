73f5bb7d176221c782fe3022060f0385899f92da
owwwl666 <zaharsalam228@gmail.com>
2023-08-01 00:33:00 +1000

    Update parse_tululu.py

--- parse_tululu.py
+++ parse_tululu.py
@@ -19,8 +19,8 @@ def parse_book_page(html_content, book_link):
 
     Возвращает словарь с основной информацией о книге.
     """
-    soup = BeautifulSoup(html_content, 'lxml')
-    book = soup.find(id="content").find("h1").text.split('::')
+    soup = BeautifulSoup(html_content, "lxml")
+    book = soup.find(id="content").find("h1").text.split("::")
     book_name, book_author = book[0].strip(), book[1].strip()
     book_image = urljoin(
         book_link,
@@ -40,22 +40,22 @@ def parse_book_page(html_content, book_link):
 def download_txt(book_id, book_name, folder):
     """Скачивает текст книги."""
     name = sanitize_filename(book_name)
-    book_path = Path(folder).joinpath(f"{book_id}.{name}.txt")
+    book_path = Path(folder).joinpath(f'{book_id}.{name}.txt')
     return book_path
 
 
 def download_image(book_image, folder):
     """Скачивает обложку книги."""
     image_download = requests.get(book_image)
-    image_name = book_image.split('/')[-1]
-    book_image_path = Path(folder).joinpath(f"{image_name}")
+    image_name = book_image.split("/")[-1]
+    book_image_path = Path(folder).joinpath(f'{image_name}')
     with open(book_image_path, 'wb') as file:
         file.write(image_download.content)
 
 
 def download_comment(book_id, book_name, book_comments, folder):
     """Скачивает отзывы и комментарии о книге."""
-    book_comments_path = Path(folder).joinpath(f"{book_id}.{book_name}.txt")
+    book_comments_path = Path(folder).joinpath(f'{book_id}.{book_name}.txt')
     for book_comment in book_comments:
         with open(book_comments_path, 'a') as file:
             file.write(f'{book_comment.find(class_="black").text}\n')
