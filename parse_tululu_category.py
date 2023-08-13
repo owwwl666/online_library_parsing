002c208d9fbbbba4810d9ad4a504238920272b0b
owwwl666 <zaharsalam228@gmail.com>
2023-08-13 13:42:59 +1000

    Update parse_tululu_category.py
    added the ability for the user to set the directory for downloading books and the ability not to download them.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -33,13 +33,18 @@ parser.add_argument("--start_page", type=int, required=False, default=1,
                     help="Целочисленный аргумент (начальная страница)")
 parser.add_argument("--end_page", type=int, required=False, default=last_page,
                     help="Целочисленный аргумент (конечная страница)")
+parser.add_argument("--dest_folder", required=False, type=str,
+                    help="Путь к каталогу с результатами парсинга:(по-умолчанию корень проекта)")
+parser.add_argument("--skip_imgs", help='Скачивать ли обложки книг', action="store_true")
+parser.add_argument("--skip_txt", help='Скачивать ли тексты книг', action="store_true")
 
 args = parser.parse_args()
 
 paths = {
-    "books_path": env.str("BOOKS"),
-    "images_path": env.str("IMAGES"),
-    "comments_path": env.str("COMMENTS"),
+    "books_path": Path(args.dest_folder).joinpath('books_text') if args.dest_folder else Path.cwd().joinpath(
+        'books_text'),
+    "images_path": Path(args.dest_folder).joinpath('book_images') if args.dest_folder else Path.cwd().joinpath(
+        'book_images'),
 }
 
 for path in paths:
@@ -74,10 +79,11 @@ for page in range(args.start_page, args.end_page + 1):
                 html_content_book = book_response.text
 
                 parse_book = parse_book_page(html_content_book, book_url)
-                saves_image(
-                    parse_book["cover"],
-                    paths["images_path"]
-                )
+                if not args.skip_imgs:
+                    saves_image(
+                        parse_book["cover"],
+                        paths['images_path']
+                    )
 
             except requests.exceptions.HTTPError:
                 logging.error(f'Книги по {book_id}-ому id не существует')
@@ -85,18 +91,18 @@ for page in range(args.start_page, args.end_page + 1):
                 logging.error(f'Не установлено соединение с сервером')
                 time.sleep(10)
             else:
-                saves_txt(
-                    book_txt_response.content,
-                    book_id,
-                    parse_book['header'],
-                    paths['books_path']
-                )
+                if not args.skip_txt:
+                    saves_txt(
+                        book_txt_response.content,
+                        book_id,
+                        parse_book['header'],
+                        paths['books_path']
+                    )
 
                 book_comments_path = saves_comments(
                     book_id,
                     parse_book["header"],
                     parse_book["comments"],
-                    paths["comments_path"]
                 )
 
                 book_genres_path = saves_genres(
@@ -109,11 +115,14 @@ for page in range(args.start_page, args.end_page + 1):
                         'title': parse_book["header"],
                         'author': parse_book['author'],
                         'img_src': parse_book['cover'],
-                        'book_path': str(Path(paths['books_path']).joinpath(parse_book['header'])),
+                        'book_path': str(
+                            Path(paths['books_path']).joinpath(parse_book['header'])) if not args.skip_txt else None,
                         'comments': book_comments_path,
                         'genres': book_genres_path
                     }
                 )
 
-with open('books.json', 'w', encoding='utf8') as json_file:
+json_path = Path(args.dest_folder).joinpath('books.json') if args.dest_folder else Path.cwd().joinpath(
+    'books.json')
+with open(json_path, 'w', encoding='utf8') as json_file:
     json.dump(books_all_pages, json_file, ensure_ascii=False, indent=4)
