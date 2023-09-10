ef40d7c2538adf0da4ba8988ada46e9c7d57e78f
owwwl666 <zaharsalam228@gmail.com>
2023-09-11 01:50:08 +1000

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -1,25 +1,31 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server, shell
 from pathlib import Path
 import json
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('index.html')
-path = Path.cwd().joinpath('books.json')
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open(path, 'r', encoding='utf8') as json_file:
-    books = json.load(json_file)
+    template = env.get_template('template.html')
+    path = Path.cwd().joinpath('books.json')
 
-rendered_page = template.render(
-    books=books
-)
+    with open(path, 'r', encoding='utf8') as json_file:
+        books = json.load(json_file)
 
-with open('template.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        books=books
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+on_reload()
+
+server = Server()
+server.watch('template.html', on_reload)
+server.serve(root='.')
