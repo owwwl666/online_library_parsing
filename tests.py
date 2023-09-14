e044d6bb16afc4b8a681fad24d27f8c6b393a3d0
owwwl666 <zaharsalam228@gmail.com>
2023-09-14 13:54:42 +1000

    Prepare site to work offline.

---
+++ tests.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+from more_itertools import chunked
+import argparse
+
+# parser = argparse.ArgumentParser()
+# args = parser.parse_args()
+path = Path.cwd().joinpath('books.json')
+# with open(path, 'r', encoding='utf8') as json_file:
+#     books = json.load(json_file)
+
+
